333f9f0cfd8e50788637e7ae924f9d05dcf8ba41
ズ <144454064+NNazar6@users.noreply.github.com>
2024-02-05 00:35:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,154 @@
+// let num = prompt('Hello, input your num', '')
+// let mes = (num == 10) ? "True" : 'False'
+// console.log(mes);
+
+// let min = prompt('Input num from 0 to 59', '')
+// let mes = (min < 0 || min > 59) ? 'Input true num' :
+//     (min < 16) ? 'First quarter' : 
+//     (min < 31) ? 'Second quarter' :
+//     (min < 46) ? 'Third quarter' : 'Fourth quarter'
+// alert(mes)
+
+// let lang = prompt('Input "ru" or "en"', '')
+// if (lang == 'ru'){
+//     alert(['понедельник', ' вторник', ' среда', ' четверг', ' пятница', ' суббота', ' воскресенье'])
+// }if(lang == 'en'){
+//     alert([' monday', ' tuesday', ' wednsday', ' thursday', ' friday', ' saturday', ' sunday'])
+// }
+
+// let arr = prompt('Input "ru" or "en"', '')
+// switch(arr){
+//     case 'en':
+//         alert([' monday', ' tuesday', ' wednsday', ' thursday', ' friday', ' saturday', ' sunday'])
+//         break
+//     case 'ru':
+//         alert(['понедельник', ' вторник', ' среда', ' четверг', ' пятница', ' суббота', ' воскресенье'])
+// }
+
+// let lang = prompt('Input "ru" or "en"', '')
+// let arr = (lang == 'ru') ? ['понедельник', ' вторник', ' среда', ' четверг', ' пятница', ' суббота', ' воскресенье'] : [' monday', ' tuesday', ' wednsday', ' thursday', ' friday', ' saturday', ' sunday']
+// alert(arr)
+
+// let lang = prompt('Input "ru" or "en"', '')
+// let arr = {
+//     ru : ['понедельник', ' вторник', ' среда', ' четверг', ' пятница', ' суббота', ' воскресенье'],
+//     en : [' monday', ' tuesday', ' wednsday', ' thursday', ' friday', ' saturday', ' sunday']
+// }
+// console.log(arr[lang])
+// let a = prompt('Input the coolest option', '1/0/-3');
+// alert(a)
+// if (a==0){
+//     console.log('True')
+// }else {
+//     console.log('Else')
+// }
+
+// let a = prompt('Input the coolest option', '1/0/-3');
+// alert(a)
+// if (a>0){
+//     console.log('True')
+// }else {
+//     console.log('Else')
+// }
+
+// let a = prompt('Input the collest option', '1/0/-3');
+// if (a<0){
+//     console.log('True')
+// } else {
+//     console.log('Else')
+// }
+
+// let a = prompt('Input the coolest option', '1/0/-3')
+// if (a>=0){
+//     console.log('True');
+// } else {
+//     console.log('Else')
+// }
+
+// let a = prompt('Input the coolest options', '1/0/-3')
+// if (a<= 0){
+//     console.log('True')
+// } else {
+//     console.log('Else')
+// }
+
+// let a = prompt('Input the coolest options', '1/0/-3')
+// if (a != 0){
+//     console.log('True')
+// } else {
+//     console.log('Else')
+// }
+
+// let a = prompt('Input word test', '')
+// if (a == 'test'){
+//     console.log('True')
+// } else {
+//     console.log('Else')
+// }
+
+// let a = prompt('Input 1', '')
+// if (a == 1 && typeof a == typeof 1){
+//     console.log('True')
+// } else {
+//     console.log('Else')
+// }
+
+// let a = prompt('Input a number', '5/0/-3/2')
+// if (a>0 && a<5){
+//     console.log('True');
+// } else {
+//     console.log('Else')
+// }
+
+// let a = prompt('Input a number', '5/0/-3/2')
+// if (a==2 || a==0){
+//     console.log(Number(a) + 7);
+// } else {
+//     console.log(a/10);
+// }
+
+// let a = prompt('Input a number', '5/0/-3/2'),
+//     b = prompt('Input a number', '5/0/-3/2');
+// if (a == 1 || a < 1 && b > 3 || b == 3){
+//     console.log(Number(a) + Number(b));
+// } else {
+//     console.log(a - b)
+// }
+
+// let a = prompt('Input a number', '5/0/-3/2'),
+//     b = prompt('Input a number', '5/0/-3/2');
+// if (a > 2 && a < 11 || b >= 6 && b < 14){
+//     console.log('True');
+// } else {
+//     console.log('Else')
+// }
+
+//  let a = prompt('Input a number', '')
+//  switch(a){
+//     case '1':
+//         console.log('winter');
+//         break;
+
+//     case '2':
+//         console.log('spring')
+//         break;
+
+//     case '3':
+//         console.log('summer')
+//         break;
+
+//     case '4':
+//         console.log('autumn')
+//         break;
+//  }
+
+// let day = prompt('1-31', '')
+// let mes = (day <= 0) ? alert('False num') :
+//     (day < 11) ? alert('first dekada month') :
+//     (day < 21) ? alert('second dekada month') :
+//     (day < 31) ? alert('thrid dekada month') :
+    
+// console.log(mes)
+
+// let str = prompt('Input six-digit num', '')
+// let mes = (str[0] + str[1] + str[2] == str[3] + str[4] + str[5]) ? alert('True') : alert('False')
